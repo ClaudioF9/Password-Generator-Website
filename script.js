ee86e6138b564b12091738ee1c84c91938987c08
ClaudioF9 <claudioferreira0284@gmail.com>
2023-01-17 17:16:17 +0000

    Created final function that uses previous two functions to generate password. Added and cleaned up older comments

--- script.js
+++ script.js
@@ -88,23 +88,23 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// // Function to prompt user for password options - this is the selections of choices the users have made
+// // Function to prompt user for password options - this is the selections of choices the user has made
 function getPasswordOptions() {
     let passLength = prompt("How many characters would you like in your password? (between 10 and 64 characters):");
     
-    // Validate password length
+    //a while statement is used to make sure the character amount falls within the specified limit and repeats if the incorrect value is typed
     while (passLength < 10 || passLength > 64) {
     alert("Password length must be between 10 and 64 characters. Please try again.");
-    passLength = prompt("How many characters woukld you like in your password? (between 10 and 64 characters):");
+    passLength = prompt("How many characters would you like in your password? (between 10 and 64 characters):");
     }
     
-    // Prompt for character types to include in password
+    // Prompt for character types to include in password - these are boolean 
     let lowercase = confirm("Include lowercase characters in the password?");
     let uppercase = confirm("Include uppercase characters in the password?");
     let numeric = confirm("Include numeric characters in the password?");
     let special = confirm("Include special characters in the password?");
     
-    // Validate at least one character type is selected
+    //a while statement is used to make sure at least one character type is selected
     while (!lowercase && !uppercase && !numeric && !special) {
     alert("You must select at least one character type for the password. Please try again.");
     lowercase = confirm("Include lowercase characters in the password?");
@@ -125,25 +125,41 @@ function getRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//calling the function
-
-console.log(getRandom(["snorker", "bird", "giraffe", "cat", "dog"]));
-
-
-// }
+//calling the function to test if working
+// console.log(getRandom(["snorker", "bird", "giraffe", "cat", "dog"]));
 
 // // Function to generate password with user input - bring together all logic above to generate passowrd
-// function generatePassword() {
+function generatePassword() {
+//pull prompt information from first function to create object
+  let options = getPasswordOptions();
 
-//     //this is the variable that will be used for the new password
-// let password = ""
+//create empty array to received prompt selected characters
+  let newPasswordArray = [];
 
-// return password
-
-// }
-
-
+//If statements, if confirms prompts are true then global array will be concatenated into the new array
+  if (options.lowercase) {
+    newPasswordArray = newPasswordArray.concat(lowerCasedCharacters);
+  }
+  if (options.uppercase) {
+    newPasswordArray = newPasswordArray.concat(upperCasedCharacters);
+  }
+  if (options.numeric) {
+    newPasswordArray = newPasswordArray.concat(numericCharacters);
+  }
+  if (options.special) {
+    newPasswordArray = newPasswordArray.concat(specialCharacters);
+  }
   
+  let password = "";
+
+  //a for loop is run with the amount of loops based on the length originally selected, it then uses the newly created array and uses the 2nd function to randomise each character which is then added to the password variable
+  for (let i = 0; i < options.passLength; i++) {
+    password += getRandom(newPasswordArray);
+  }
+  
+  return password;
+}
+
   // Get references to the #generate element
   var generateBtn = document.querySelector('#generate');
   
